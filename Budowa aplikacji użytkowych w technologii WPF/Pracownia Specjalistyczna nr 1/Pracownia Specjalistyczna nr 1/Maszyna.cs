fe4f641ac873940b64ba52f36ef124eb3ba816d6
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-07-11 11:39:32 +0200

    Repository refresh

---
+++ Budowa aplikacji użytkowych w technologii WPF/Pracownia Specjalistyczna nr 1/Pracownia Specjalistyczna nr 1/Maszyna.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracownia_Specjalistyczna_nr_1
+{
+    internal class Maszyna
+    {
+        private List<string> list = new List<string>();
+        Random rnd;
+
+        public Maszyna()
+        {
+            rnd = new Random();
+        }
+
+        public void Dodaj(string item)
+        {
+            list.Add(item);
+        }
+
+        public string Losuj()
+        {
+            int number = rnd.Next(0, list.Count);
+            string tekst = list[number];
+            list.Remove(tekst);
+            return tekst;
+        }
+
+        public bool Kupony()
+        {
+            if (list.Count == 0) return false;
+            else return true;
+        }
+
+        public List<string> ListKupony()
+        {
+            return list;
+        }
+    }
+}
